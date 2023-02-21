914fb5102aaaf49fa0748aec155d96e2a34c53b7
NicholasGlut <112663136+NicholasGlut@users.noreply.github.com>
2023-02-20 23:56:00 -0500

    Update Temperature_Converter.py

--- Temperature_Converter.py
+++ Temperature_Converter.py
@@ -1,24 +1,25 @@
-# Display two messages
 import unittest
 
+print("This is a program to convert units of temperature.")
 
-print("Welcome to Python")
-print("Python is fun")
-
-print("Input F for Farenheit or C for Celsius: ")
-unit = input()
-print(unit.upper())
-if unit.upper() == "F":
-    print("You selected Farenheit.")
-elif unit.upper() == "C":
-    print("You selected Celsius.")
-else:
-    print("Please select F or C...")
+while true:
+    print("Type EXIT to exit...")
+    print("\nInput F for Farenheit or C for Celsius: ")
     unit = input()
+    print(unit.upper())
+    
+    if unit.upper() == "F":
+        print("You selected Farenheit.")
+    elif unit.upper() == "C":
+        print("You selected Celsius.")
+    else:
+        print("Please select F or C...")
+        unit = input()
 
-if unit.upper() == "F":
-    temp = int(input("Please input temperature Farenheit: "))
-    print((5/9) * (temp-32))
-elif unit.upper() == "C":
-    temp = int(input("Please input temperature Celsius: "))
-    print((9/5)*temp+32)
+    if unit.upper() == "F":
+        temp = int(input("Please input temperature Farenheit: "))
+        print((5/9) * (temp-32))
+    elif unit.upper() == "C":
+        temp = int(input("Please input temperature Celsius: "))
+        print((9/5)*temp+32)
+    
