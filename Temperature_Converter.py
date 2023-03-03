b600d39435c2e028707b72cf4285bc161f57eee7
NicholasGlut <112663136+NicholasGlut@users.noreply.github.com>
2023-03-02 19:15:40 -0500

    Update Temperature_Converter.py

--- Temperature_Converter.py
+++ Temperature_Converter.py
@@ -1,12 +1,10 @@
-import unittest
 
 print("\nThis is a program to convert units of temperature.")
 
 while True:
     # User chooses the starting unit of temperature
     print("\nType EXIT to exit...")
-    print("Input F for Farenheit or C for Celsius: ")
-    unit = input()
+    unit = input("Input F for Farenheit or C for Celsius: ")
     
     # Allows exiting of program
     if unit.upper() == 'EXIT':
@@ -23,15 +21,15 @@ while True:
         unit = input()
 
     # Applies temperature conversion
-    if unit.upper() == "F":
-        temp = int(input("Please input temperature Farenheit: "))
-        if temp.upper() == 'EXIT':
-        break
-        print((5/9) * (temp-32))
+    while True:
+        if unit.upper() == "F":
+            temp = int(input("Please input temperature Farenheit: "))
+            if str(temp).upper() == 'EXIT':
+                break
+            print((5/9) * (temp-32))
 
-    elif unit.upper() == "C":
-        temp = int(input("Please input temperature Celsius: "))
-        if temp.upper() == 'EXIT':
-        break
-        print((9/5)*temp+32)
-    
+        elif unit.upper() == "C":
+            temp = int(input("Please input temperature Celsius: "))
+            if str(temp).upper() == 'EXIT':
+                break
+            print((9/5)*temp+32)
